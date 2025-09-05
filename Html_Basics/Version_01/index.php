5e825b06243c093d1a16bf31fb9e59aa4b08273a
tyler.hooley <tylerhooley77@gmail.com>
2025-09-05 13:13:27 +0100

    testing tables things and images

--- Html_Basics/Version_01/index.php
+++ Html_Basics/Version_01/index.php
@@ -1,12 +1,40 @@
 <?php //this opens the php code section
 
+echo "<!DOCTYPE html>";  // desired tag to declare what type of page it is
+
 echo "<html>";  // opening html
 echo "<head>";  // opening head
+
 echo "<title>tyler</title>";  // creating title
+
 echo "</head>";
 echo "<body>"; // opening body
+
 echo "<h1>Hello World!</h1>";  // creating header
 echo "<p>Hello</p>";  // creating paragraph
+echo "<hr>";
+echo "<p>Hello</p>";  // creating paragraph
+echo "<br>";
+echo "<p>Hello</p>";  // creating paragraph
+echo "<i>Hello-i</i>";  // creating paragraph
+echo "<em>Hello-em</em>";  // creating paragraph
+echo "<p><strong>Hello-strong</strong></p>";  // creating paragraph
+echo "<b>Hello-b</b>";  // creating paragraph
+echo "<table>";
+echo "<tr>";
+    echo "<th>Hello1</th>";
+    echo "<th>Hello2</th>";
+    echo "<th>Hello3</th>";
+echo "</tr>";
+echo "<tr>";
+    echo "<th>Hello4</th>";
+    echo "<th>Hello5</th>";
+    echo "<th>Hello6</th>";
+echo "</tr>";
+echo "</table>";
+echo "<img src='image.jfif'>";
+echo "<a href='page2.php'>Go to page 2</a>";
 echo "</body>";
+
 echo "</html>";
 ?>
