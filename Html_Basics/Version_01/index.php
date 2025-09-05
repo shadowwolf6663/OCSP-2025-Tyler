f66beb3e3fa8b10ff1795faea2403c86c07bd3a7
tyler.hooley <tylerhooley77@gmail.com>
2025-09-05 10:37:34 +0100

    creating my first php webpage

---
+++ Html_Basics/Version_01/index.php
@@ -0,0 +1,12 @@
+<?php //this opens the php code section
+
+echo "<html>";  // opening html
+echo "<head>";  // opening head
+echo "<title>tyler</title>";  // creating title
+echo "</head>";
+echo "<body>"; // opening body
+echo "<h1>Hello World!</h1>";  // creating header
+echo "<p>Hello</p>";  // creating paragraph
+echo "</body>";
+echo "</html>";
+?>
