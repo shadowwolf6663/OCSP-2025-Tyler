9ff464655cc6296e5a993af4262b71898094bf49
tyler.hooley <tylerhooley77@gmail.com>
2025-09-12 14:32:13 +0100

    added some login features and booking and better visulisation to design

--- Html_Basics/mini_project_02/version_01/assets/topbar.php
+++ Html_Basics/mini_project_02/version_01/assets/topbar.php
@@ -2,6 +2,7 @@
 echo "<div class='topbar'>";
 echo "<tr>";
 echo "<h1>Gibjohn Tutoring</h1>";  // creating header
+echo "<img src='images/logo.jpg' alt='logo' id='logo'>";
 echo "</tr>";
 echo "</div>";
 ?>
--- Html_Basics/mini_project_02/version_01/index.php
+++ Html_Basics/mini_project_02/version_01/index.php
@@ -17,7 +17,13 @@ echo "<div class ='container'>";
     require_once "assets/nav.php";
 
 echo "<div class ='content'>";
-
+echo "<p>Welcome to Gibjohn Tutoring!
+Helping You Learn, Grow, and Succeed—One Step at a Time
+At Gibjohn Tutoring, we’re all about making learning easier, more enjoyable, and more personal. Whether you’re struggling with a tricky subject, preparing for exams, or just want to stay ahead in class, we’re here to support you every step of the way.
+We offer a growing library of learning resources that you can access anytime—perfect for brushing up on topics at your own pace. Need a bit more help? You can also book face-to-face tutoring sessions with friendly, experienced tutors who know how to explain things in a way that just makes sense.
+No pressure. No confusing jargon. Just real help from people who care about your progress.
+Whether you’re a student looking to boost your grades, a parent searching for extra support for your child, or an adult learner getting back into education, Gibjohn Tutoring is here for you.
+So take a look around, explore our resources, and when you're ready, book a session with one of our tutors. Let's work together to reach your goals—whatever they are.</p>";
 
 
 
